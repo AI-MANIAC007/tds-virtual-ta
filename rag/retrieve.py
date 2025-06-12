24888b80f7f67f127c6794ca25249c53da34f79c
AI-MANIAC007 <hemanthraja0912@gmail.com>
2025-06-12 23:28:44 +0530

    Update retrieve.py

--- rag/retrieve.py
+++ rag/retrieve.py
@@ -5,7 +5,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 import os
 
-os.environ["AIPROXY_TOKEN"] = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjIwMDE5MTVAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.yyL_AkgLzYF6sRRlwrAPZHWRpDX6qvHdBRFisQVRaf4"
+os.environ["AIPIPE_TOKEN"] = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjIwMDE5MTVAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.FdixSrNdglNv2jOA78f6NDwH8BxXLMmP2tYpehHhVn4"
 
 def get_answer(query):
     db = FAISS.load_local("rag/index", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
